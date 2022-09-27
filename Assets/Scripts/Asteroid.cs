60b04f40dd82adbe18ab2945ca63794c9182590b
Rob Kayson <rkayson@gmail.com>
2022-09-26 17:20:02 -0700

    Add asteroid prefab with asteroid shader graph material

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+  public Vector3 velocity = Vector3.zero;
+  public Vector3 rotationSpeed = Vector3.zero;
+
+  void FixedUpdate()
+  {
+    transform.position += velocity * Time.deltaTime;
+    transform.Rotate(rotationSpeed.x * Time.deltaTime, rotationSpeed.y * Time.deltaTime, rotationSpeed.z * Time.deltaTime);
+  }
+}
