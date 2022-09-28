a17207256ea4617c6dd15c9e259181bd500048bf
faircall <cooper.willis@gmail.com>
2022-09-29 00:17:36 +1000

    fixed heading on turret

--- Assets/Scripts/TurretBehaviour.cs
+++ Assets/Scripts/TurretBehaviour.cs
@@ -16,6 +16,7 @@ public class TurretBehaviour : MonoBehaviour
     private bool playerInRange;
 
     float timeBetweenShots = 0.75f;
+    private float aimSpeed = 0.035f; // percent per fixed update frame
     private float shotTimer;
 
     private bool canShoot;
@@ -24,38 +25,30 @@ public class TurretBehaviour : MonoBehaviour
     {
         canShoot = true;
         shotTimer = 0.0f;
-
-        
-
-        
-        // just take from the rotation
-        heading = transform.up;
-        playerInRange = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // do a bunch of work to a get a valid range for shooting
-        heading = transform.up;
-        playerInRange = false;
-        Vector3 playerPos = PlayerControl.instance.transform.position;
-        Vector3 castToPlayer = playerPos - transform.position;
+        heading = transform.forward;
+        playerInRange = false;        
+        Vector3 dirToPlayer = PlayerControl.instance.transform.position - transform.position;
         // Debug.Log("heading is " + heading.x + " x " + heading.y + " y " + heading.z + " z");
-        float angle = Vector3.Angle(castToPlayer, heading);
-        Debug.Log("angle between turrent and player is " + angle);
+        float angle = Vector3.Angle(dirToPlayer, heading);
+        // Debug.Log("angle between turrent and player is " + angle);
         if ((angle) > maxTurretAngle) {
             // Debug.Log("Can't hit shoot player");
         } else {
             // not taking into account actual visibility/occlusion yet
             playerInRange = true;
-            Debug.Log("Can hit player");
+            // Debug.Log("Can hit player");
         }
 
         if (playerInRange && canShoot) {
             // actually shoot
             // gonna need a proper direction (that of the player) to pass as a Quaternion rotation?
-            Debug.Log("launching shot");
+            // Debug.Log("launching shot");
             canShoot = false;
             GameObject shotGO = GameObject.Instantiate(turretShotPrefab, transform.position, transform.rotation);
         }
@@ -67,7 +60,14 @@ public class TurretBehaviour : MonoBehaviour
                 shotTimer = 0.0f;
             }
         }
+    }
 
-
+    void FixedUpdate() // slerp uses % so can't be in update which is framerate dependent
+    {
+        Vector3 dirToPlayer = PlayerControl.instance.transform.position - transform.position;
+        Quaternion targetAng = Quaternion.LookRotation(dirToPlayer);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetAng, aimSpeed);
     }
 }
+
+
