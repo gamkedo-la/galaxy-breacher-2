a2cf6403f7d954fe57bc21d2b60a9b0e8a8582ad
Michael Fewkes <mdfewkes@gmail.com>
2022-10-02 00:50:11 -0700

    Integrated barometer detection into explosions

--- Assets/Scripts/ShrinkConstantly.cs
+++ Assets/Scripts/ShrinkConstantly.cs
@@ -6,8 +6,15 @@ public class ShrinkConstantly : MonoBehaviour
 {
     float scaleAfterEachFixedTick = 0.98f;
 
-    void FixedUpdate()
+    private BarometerDetectable baroDetect;
+
+	void Start() {
+        baroDetect = GetComponent<BarometerDetectable>();
+	}
+
+	void FixedUpdate()
     {
         transform.localScale *= scaleAfterEachFixedTick;
+        if (baroDetect) baroDetect.size *= scaleAfterEachFixedTick;
     }
 }
