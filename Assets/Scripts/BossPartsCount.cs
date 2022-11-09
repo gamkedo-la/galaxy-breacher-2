72bd0905ac6c9428b1ad0fd9658f35ce34a629a9
Joshua <xluciela@gmail.com>
2022-11-09 09:44:55 -0500

    Added the counter for boss parts

--- Assets/Scripts/BossPartsCount.cs
+++ Assets/Scripts/BossPartsCount.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BossPartsCount : MonoBehaviour
 {
-    public int bossPartsNumber;
+    private int bossPartsNumber;
+    int parts;
     
+    public TextMeshProUGUI partsNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        parts = GameObject.FindGameObjectsWithTag("BossPart").Length;
+        partsNumber.GetComponent<TextMeshProUGUI>().text = "Boss Parts: " + parts;
+
     }
 }
