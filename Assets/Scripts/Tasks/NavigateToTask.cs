9b7398471b154d93165316294ffb00958d3cf237
Maxim Kulkin <rottenhowler@gmail.com>
2022-11-10 12:29:37 -0500

    Add task to move ship to given destination

---
+++ Assets/Scripts/Tasks/NavigateToTask.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateToTask : Task
+{
+    private const float THRESHOLD_DISTANCE = 5f;
+    private const float THRESHOLD_ANGLE = 5;
+
+    private Ship ship;
+    private Vector3 targetPosition;
+    private Quaternion targetOrientation;
+    private float maxSpeed;
+
+    public NavigateToTask(Ship ship, Vector3 targetPosition, Quaternion targetOrientation)
+        : this(ship, targetPosition, targetOrientation, ship.maxSpeed)
+    {
+    }
+
+    public NavigateToTask(Ship ship, Vector3 targetPosition, Quaternion targetOrientation, float maxSpeed)
+    {
+        this.ship = ship;
+        this.targetPosition = targetPosition;
+        this.targetOrientation = targetOrientation;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        ship.NavigateTo(targetPosition, targetOrientation, maxSpeed);
+    }
+
+    public override void Cancel()
+    {
+        ship.Stop();
+        base.Cancel();
+    }
+
+    public override void Update()
+    {
+        // if ((Vector3.Distance(ship.transform.position, targetPosition) < THRESHOLD_DISTANCE) &&
+        //     (Quaternion.Angle(ship.transform.rotation, targetOrientation) < THRESHOLD_ANGLE))
+        if ((Vector3.Distance(ship.transform.position, targetPosition) < THRESHOLD_DISTANCE))
+        {
+            Completed();
+        }
+    }
+
+    public override string ToString()
+    {
+        return "NavigateToTask(" + targetPosition + ")";
+    }
+}
