1b1034328eb1ac9c57378515c5ed8a2d16bacc29
Patrick <Patrick@moffett.dev>
2022-11-13 10:24:36 -0800

    Added Health Component and Damage Interface

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamagable
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float health;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+
+    public void TakeDamage(float damageToTake)
+    {
+        health -= damageToTake;
+        if (health < 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
