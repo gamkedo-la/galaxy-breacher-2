97bc1af6b7f11d58027d25fda6d1e37b873f0c84
Maxim Kulkin <rottenhowler@gmail.com>
2022-12-11 13:48:09 -0500

    Enable formation move in NavigationTest scene

--- Assets/Scenes/NavigationTest/NavigationTestController.cs
+++ Assets/Scenes/NavigationTest/NavigationTestController.cs
@@ -17,5 +17,13 @@ public class NavigationTestController : MonoBehaviour
 
         Task task = new TopLevelTask(new ChaseTask(leader, target));
         task.Start();
+
+        FormationFollowTask followTask = new FormationFollowTask(leader, new LineFormation(50f));
+        for (int i=1; i < ships.Length; i++)
+        {
+            followTask.AddFollower(ships[i]);
+        }
+        Task task2 = new TopLevelTask(followTask);
+        task2.Start();
     }
 }
