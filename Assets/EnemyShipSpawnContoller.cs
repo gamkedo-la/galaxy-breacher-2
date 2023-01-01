20381dfb0adf385ac7f1754c363f9eaa41db0b14
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2023-01-01 12:44:47 +0300

    Added a new prefab for enemy spawn logic. At this point
    At this point, it relies on "ExplosionSelfRemove". It can be changed later on.

---
+++ Assets/EnemyShipSpawnContoller.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShipSpawnContoller : MonoBehaviour
+{
+    [Header("Spawn Objects")]
+    public GameObject shipToSpawn;
+    public Transform spawnParent;//since this will be attached to the main ship, and main ship will be able to move. We should have a separate parent.
+    public GameObject spawnPoint;
+
+    [Header("Spawn Parameters")]
+    public float secondsBetweenEverySpawn = 1.0f;
+    public int maximumNumberOfShips = 3;
+    public int currentNumberOfShips = 0;
+
+    private float timePassedAfterPreviousSpawn = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTime();
+        CheckForSpawnShip();
+    }
+
+    private void CheckForSpawnShip()
+    {
+        if (timePassedAfterPreviousSpawn == secondsBetweenEverySpawn && currentNumberOfShips <= maximumNumberOfShips)
+        {
+            SpawnShip();
+        }
+    }
+
+    private void UpdateTime()
+    {
+        if (timePassedAfterPreviousSpawn >= secondsBetweenEverySpawn)
+        {
+            timePassedAfterPreviousSpawn = secondsBetweenEverySpawn;
+        }
+        else
+        {
+            timePassedAfterPreviousSpawn += Time.deltaTime;
+        }
+    }
+
+    private void SpawnShip()
+    {
+        GameObject spawnedShip = Instantiate(shipToSpawn, spawnPoint.transform.position, spawnPoint.transform.rotation, spawnParent);
+        ExplosionSelfRemove explosionSelfRemove;
+        spawnedShip.TryGetComponent(out explosionSelfRemove);
+        if (explosionSelfRemove)
+        {
+            explosionSelfRemove.enemyShipSpawnContoller = this;
+        }
+        currentNumberOfShips++;
+        timePassedAfterPreviousSpawn = 0.0f;
+    }
+
+    public void RemoveShip()
+    {
+        if (currentNumberOfShips > 0)
+        {
+            currentNumberOfShips--;
+            timePassedAfterPreviousSpawn = 0.0f;
+        }
+    }
+}
--- Assets/Scripts/ExplosionSelfRemove.cs
+++ Assets/Scripts/ExplosionSelfRemove.cs
@@ -7,10 +7,15 @@ public class ExplosionSelfRemove : MonoBehaviour
     [SerializeField] float destroyTimer = 2.0f;
     [SerializeField] GameObject ExplosionVFX;
     [SerializeField] Transform ExplosionPosition;
+    public EnemyShipSpawnContoller enemyShipSpawnContoller;
 
     public void Remove()
     {
         GameObject explosion = Instantiate(ExplosionVFX, transform.position, Quaternion.identity);
+        if (enemyShipSpawnContoller)
+        {
+            enemyShipSpawnContoller.RemoveShip();
+        }
         Destroy(gameObject, destroyTimer);
     }
 }
