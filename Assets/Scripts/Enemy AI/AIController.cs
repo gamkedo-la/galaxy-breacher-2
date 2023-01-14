1ff987a62267dbe59e76c8d8daa77b3aa8065277
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-01-15 04:19:36 +0500

    Ship Distance Updated

--- Assets/Scripts/Enemy AI/AIController.cs
+++ Assets/Scripts/Enemy AI/AIController.cs
@@ -43,14 +43,16 @@ public class AIController : MonoBehaviour
 
         Vector3 targetDirection = (target.position - transform.position).normalized;
 
-        float targetAngleY = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+
+        //float targetAngleY = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
         float targetAngleZ = Mathf.Atan2(targetDirection.x, targetDirection.y) * Mathf.Rad2Deg;
 
         //using Mathf.MoveTowardsAngle to smoothly rotate the z-rotation value of the enemy ship towards the target angle
-        float yRotation = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngleY, rotationSpeed * Time.deltaTime);
+        // float yRotation = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngleY, rotationSpeed * Time.deltaTime);
         float zRotation = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngleZ, rotationSpeed * Time.deltaTime);
+
         //assigning the new rotation to the enemy ship's transform
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, zRotation);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, zRotation);
     }
 
 }
