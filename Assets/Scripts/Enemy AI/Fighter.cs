499d71ef39bc2b4098651f9b190c9feb2b47422f
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-01-18 02:51:08 +0500

    Setup Fighter Script for enemy ship turret behaviour

---
+++ Assets/Scripts/Enemy AI/Fighter.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 heading; // this comes by setting the 'TurretHeading' sphere in the direction you want the turret to face
+
+    public float maxTurretAngle = 90.0f;
+
+    private LineRenderer myLaserLine;
+
+    public GameObject turretShotPrefab;
+    private Vector3 originalCenterOrientation;
+    private bool playerInRange;
+
+    float timeBetweenShots = 0.75f;
+    private float aimSpeed = 0.035f; // percent per fixed update frame
+    private float shotTimer;
+
+    private bool canShoot;
+
+
+
+    void Start()
+    {
+        originalCenterOrientation = transform.forward;
+        canShoot = true;
+        shotTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // do a bunch of work to a get a valid range for shooting
+        heading = transform.forward;
+        playerInRange = false;
+        Vector3 dirToPlayer = PlayerControl.instance.transform.position - transform.position;
+        // Debug.Log("heading is " + heading.x + " x " + heading.y + " y " + heading.z + " z");
+        float angle = Vector3.Angle(dirToPlayer, heading);
+        // Debug.Log("angle between turrent and player is " + angle);
+        if ((angle) > maxTurretAngle)
+        {
+            // Debug.Log("Can't hit shoot player");
+        }
+        else
+        {
+            // not taking into account actual visibility/occlusion yet
+            playerInRange = true;
+            // Debug.Log("Can hit player");
+        }
+
+        if (playerInRange && canShoot)
+        {
+            // actually shoot
+            // gonna need a proper direction (that of the player) to pass as a Quaternion rotation?
+            // Debug.Log("launching shot");
+            canShoot = false;
+            GameObject shotGO = GameObject.Instantiate(turretShotPrefab, transform.position, transform.rotation);
+        }
+
+
+        if (!canShoot)
+        {
+            shotTimer += Time.deltaTime;
+            if (shotTimer >= timeBetweenShots)
+            {
+                canShoot = true;
+                shotTimer = 0.0f;
+            }
+        }
+    }
+
+    void FixedUpdate() // slerp uses % so can't be in update which is framerate dependent
+    {
+        Vector3 dirToPlayer = PlayerControl.instance.transform.position - transform.position;
+        if (Vector3.Angle(dirToPlayer, originalCenterOrientation) > 85.0f)
+        {
+            // Debug.Log("outside shootable range");
+            return;
+        }
+        Quaternion targetAng = Quaternion.LookRotation(dirToPlayer);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetAng, aimSpeed);
+
+    }
+}
