4c08dc062019a67670cbc197328152599e7f7b08
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-02-27 23:11:28 +0000

    [Level Design] Added AnotherAsteroid prefab, destructible. Added 1st version of configurable Asteroid Spawner in Celestial Objects for extra asteroids

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public List<GameObject> asteroids = new List<GameObject>();
+    public GameObject celestialObjectsParent;
+    public int minAsteroids = 0;
+    public int maxAsteroids = 20;
+    public int maxX;
+    public int maxY;
+    public int maxZ;    
+    public int minX;
+    public int minY;
+    public int minZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int numberOfAsteriods = Random.Range(minAsteroids, maxAsteroids);
+
+        Debug.Log("Spawining Asteroids");
+        Debug.Log("Number of Asteroids to Spawn: " + numberOfAsteriods);
+
+        for (int i = 0; i < numberOfAsteriods; i++)
+        {
+            int newX = Random.Range(minX, maxX);
+            int newY = Random.Range(minY, maxY);
+            int newZ = Random.Range(minZ, maxZ);
+
+            GameObject newAsteroid = Instantiate(asteroids[Random.Range(0, asteroids.Count)]);
+
+            newAsteroid.transform.position = new Vector3(newX, newY, newZ);
+
+            newAsteroid.transform.SetParent(celestialObjectsParent.transform);
+            
+        }
+    }
+}
