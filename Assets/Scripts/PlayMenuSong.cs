d328b304c87385154f0ad95caf3ac1ba30784670
Gamkedo <chris@gamkedo.com>
2023-03-05 02:07:47 -0500

    Starting prefab for menu music script (commented out to avoid error)

---
+++ Assets/Scripts/PlayMenuSong.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMenuSong : MonoBehaviour
+{
+    void Start()
+    {
+        //AkSoundEngine.PostEvent("Game_Music", gameObject);
+        //AkSoundEngine.SetSwitch("Gameplay_Switch","Menu", gameObject);
+    }
+}
