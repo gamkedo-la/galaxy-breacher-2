01d0d3fe3b5bc4f700e01a44056aa4ee8ee68645
Gamkedo <chris@gamkedo.com>
2023-03-05 04:11:21 -0500

    Trying rebuilding soundbanks

--- Assets/Scripts/PlayMenuSong.cs
+++ Assets/Scripts/PlayMenuSong.cs
@@ -6,7 +6,7 @@ public class PlayMenuSong : MonoBehaviour
 {
     void Start()
     {
-        //AkSoundEngine.PostEvent("Game_Music", gameObject);
+        // AkSoundEngine.PostEvent("Game_Music", gameObject);
         //AkSoundEngine.SetSwitch("Gameplay_Switch","Menu", gameObject);
     }
 }
